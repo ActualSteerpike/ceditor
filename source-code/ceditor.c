8ed3b96259e65eac13c050bb300419ff76b40968
sourdust <dj@sparkwoodand21.com>
2020-11-10 16:13:34 +0000

    escape out of search

--- source-code/ceditor.c
+++ source-code/ceditor.c
@@ -71,7 +71,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** terminal ***/
 
@@ -391,7 +391,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
 	if (E.filename == NULL) {
-		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
 	if (E.filename == NULL) {
 	editorSetStatusMessage("Save aborted");
 	return;
@@ -421,9 +421,10 @@ void editorSave() {
 
 /*** find ***/
 
-void editorFind() {
-	char *query = editorPrompt("Search: %s (ESC to cancel)");
-	if (query == NULL) return;
+void editorFindCallback(char *query, int key) {
+	if (key == '\r' || key == '\x1b') {
+		return;
+	}
 
 	int i;
 	for (i = 0; i < E.numrows; i++) {
@@ -437,9 +438,26 @@ void editorFind() {
 		}
 	}
 
-	free(query);
 }
 
+void editorFind() {
+	int saved_cx = E.cx;
+	int saved_cy = E.cy;
+	int saved_coloff = E.coloff;
+	int saved_rowoff = E.rowoff;
+	
+
+	char *query = editorPrompt("Search %s (ESC to cancel)", editorFindCallback);
+
+	if (query) {
+	free(query);
+	} else {
+		E.cx = saved_cx;
+		E.cy = saved_cy;
+		E.coloff = saved_coloff;
+		E.rowoff = saved_rowoff;
+	}
+}
 
 /*** append buffer ***/
 
@@ -585,7 +603,7 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /*** input ***/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
 	size_t bufsize = 128;
 	char *buf = malloc(bufsize);
 
@@ -601,11 +619,13 @@ char *editorPrompt(char *prompt) {
 	if (buflen != 0) buf[--buflen] = '\0';
 	} else if (c == '\x1b') {
 		editorSetStatusMessage("");
+		if (callback) callback(buf, c);
 		free(buf);
 		return NULL;
 	} else if (c == '\r') {
 		if (buflen != 0) {
 			editorSetStatusMessage ("");
+			if (callback) callback(buf, c);
 			return buf;
 		}
 	} else if (!iscntrl(c) && c < 128) {
@@ -616,6 +636,7 @@ char *editorPrompt(char *prompt) {
 	buf[buflen++] = c;
 	buf[buflen] = '\0';
 	}
+	if (callback) callback(buf, c);
 }
 }
 
